41205ba6651546702870651ef4a824aaf3bd2683
Rabire <rabireh@outlook.fr>
2021-07-07 09:28:38 +0200

    archi

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Header } from './Header';
+import { Header } from "../components/Header/Header";
 
 export default {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
 } as ComponentMeta<typeof Header>;
 
