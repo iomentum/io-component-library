b3283c8af3a9f99d35907e17a865553cb2734348
Rabire <rabireh@outlook.fr>
2021-07-09 15:24:37 +0200

    plugins removed

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
-// import image from "rollup-plugin-image";
-import images from "rollup-plugin-image-files";
 
 import packageJson from "./package.json";
 
@@ -33,6 +31,5 @@ export default {
     resolve(), // add to the source code our third party dependencies
     commonjs(), // convert CommonJS modules to ES6, so they can be included in a Rollup bundle
     typescript(),
-    images(), // Import JPG, PNG, GIF and SVG files.
   ],
 };
