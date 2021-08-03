e7030e6ec2e12c3681e27336302c335a7a5dded9
Rabire <rabireh@outlook.fr>
2021-08-03 17:12:43 +0200

    DayWrapper test event iter2

--- src/Calendar/components/DayzWrapper.test.tsx
+++ src/Calendar/components/DayzWrapper.test.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import Dayz from 'dayz';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { CalendarContext } from '../contexts/CalendarContext';
 import { EventContext } from '../contexts/EventContext';
 import { DisplayMode } from '../utils';
@@ -13,6 +13,7 @@ import { DayzWrapper } from './DayzWrapper';
 jest.mock('../utils', () => ({
   createExtendedMomentFromDate: () => new Date('2021-08-03T14:36:39.811Z'),
   DisplayMode: 'week',
+  createEvent: jest.fn(),
 }));
 
 const calendarContextMock = (component, calendarProviderValue, eventProviderValue) =>
@@ -43,6 +44,27 @@ describe('DayzWrapper component', () => {
     });
   });
 
-  // describe('@props', () => {});
-  // describe('@events', () => {});
+  describe('@events', () => {
+    it('should trigger handleDayEventClick', () => {
+      const newDate = new Date('2021-08-03');
+
+      const calendarProviderValue = { displayedDate: newDate, displayMode: DisplayMode.Week };
+      const eventProviderValue = {
+        eventsCollection: new Dayz.EventsCollection([]),
+        setCurrentEvent: jest.fn(),
+        setEventManagementOpened: jest.fn(),
+      };
+
+      calendarContextMock(
+        <DayzWrapper data-testid="dayz" />,
+        calendarProviderValue,
+        eventProviderValue
+      );
+
+      screen.getAllByText(3)[0].click();
+
+      expect(eventProviderValue.setCurrentEvent).toBeCalledTimes(1);
+      expect(eventProviderValue.setEventManagementOpened).toBeCalledWith(true);
+    });
+  });
 });
